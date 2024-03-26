668bd945ae67c92d1e1a2336ac4862b3f7a61f52
kaua-zanni <165018613+kaua-zanni@users.noreply.github.com>
2024-03-26 09:58:49 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+alert('ola');       
+
+let numeroSecreto = 5;
+let numeroEscholido = parseInt (prompt('digite um numero de 1 a 50'));
+
+if (numeroSecreto == numeroEscholido) {
+    alert('parabens vocé acertou');
+    } else if(numeroSecreto<numeroEscholido) {
+        alert('o numero  é menor');
+    } else {
+        alert('o numero é maior');
+    }
