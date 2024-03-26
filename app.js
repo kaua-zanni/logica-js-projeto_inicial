d873ee0247a3599cd1907cb6e08e1fb2d182ebdb
kaua-zanni <165018613+kaua-zanni@users.noreply.github.com>
2024-03-26 11:00:15 -0300

    up2

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 
 
-alert('ola');  // aparece pu usuario um janela de alerta     
+alert('ola');  // aparece  para o usuario um janela de alerta     
 
-let numeroSecreto = math.floor(math.random() * 50)  //math.floor serve para arendondar um numero e math.random()  *x serve para randomizar numeros                                                //usamos para declara uma varivel
-let numeroEscholido = parseInt (prompt('digite um numero de 1 a 50'));
-let numeroTentativa
+let numeroSecreto = Math.floor(Math.random() * 50)  //math.floor serve para arendondar um numero e math.random()  *x serve para randomizar numeros                                                //usamos para declara uma varivel
 
-while (numeroTentetiva<= 3 ) {
+let numeroTentativa = 0;
 
-    numeroTentativa = numeroTentativa + 1;
-  
-    if (numeroSecreto == numeroEscholido) {                   //o if é se e o else if é senao se ja o else é senao
-    alert(`parabens vocé acertou ${numeroEscholido}`);
+while (numeroTentativa <= 3 ) {
+    let numeroEscholido = parseInt(prompt('digite um numero de 1 a 50'));
+    if (numeroSecreto == numeroEscholido) {                 //o if é se e o else if é senao se ja o else é senao
+            
+        alert(`parabens vocé acertou ${numeroEscholido}`);
+        break;
          } else if(numeroSecreto<numeroEscholido) {
              alert(`o numero ${numeroEscholido} é menor do que o secreto`);   //usamos a crase (`) quando precisamos integrar uma variravel no alert
          } else {
-        alert(`o numero  ${numeroEscholido} é maior`);
+             alert(`o numero  ${numeroEscholido} é maior`);
          }
-
+         numeroTentativa = numeroTentativa + 1;
 }
