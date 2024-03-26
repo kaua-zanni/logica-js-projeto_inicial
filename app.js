0e49be4bda37b8f9db727c65fe21e32f531a58f6
kaua-zanni <165018613+kaua-zanni@users.noreply.github.com>
2024-03-26 10:16:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
 
-alert('ola');       
+alert('ola');  // aparece pu usuario um janela de alerta     
 
-let numeroSecreto = 5;
+let numeroSecreto = 5;                                                      //usamos para declara uma varivel
 let numeroEscholido = parseInt (prompt('digite um numero de 1 a 50'));
 
-if (numeroSecreto == numeroEscholido) {
-    alert('parabens vocé acertou');
+if (numeroSecreto == numeroEscholido) {                   //o if é se e o else if é senao se ja o else é senao
+    alert(`parabens vocé acertou ${numeroSecreto}`);
     } else if(numeroSecreto<numeroEscholido) {
         alert('o numero  é menor');
     } else {
