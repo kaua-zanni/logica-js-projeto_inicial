3f4394b16b06398c37996d50fd7235ab6ea8dba8
kaua-zanni <165018613+kaua-zanni@users.noreply.github.com>
2024-03-27 11:24:33 -0300

    or

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ alert('ola');
 
 let numeroSecreto=Math.floor(Math.random()*50);
 
-
 for (let numeroTentativa = 0;numeroTentativa<=10; numeroTentativa++) {
 
         let numeroEscholido = parseInt(prompt('digite um numero de 1 a 50'));
 
         if(numeroEscholido==numeroSecreto){
    
-            alert('parabens vocé acertou o numero escholido')
-            break
+            alert('parabens vocé acertou o numero escholido');
+            break;
 
         } else if (numeroSecreto<numeroEscholido){
               
