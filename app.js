02dd9540feedad978d18b8d246790cb138e6c936
kaua-zanni <165018613+kaua-zanni@users.noreply.github.com>
2024-03-27 11:11:21 -0300

    kk

--- app.js
+++ app.js
@@ -3,24 +3,23 @@ alert('ola');
 
 let numeroSecreto=Math.floor(Math.random()*50);
 
+
 for (let numeroTentativa = 0;numeroTentativa<=10; numeroTentativa++) {
 
-    let numeroEscholido = parseInt(prompt('digite um numero de 1 a 50'));
+        let numeroEscholido = parseInt(prompt('digite um numero de 1 a 50'));
 
-         if(numeroEscholido==numeroSecreto){
-
+        if(numeroEscholido==numeroSecreto){
+   
             alert('parabens vocé acertou o numero escholido')
             break
 
-         } else if (numeroSecreto<numeroEscholido){
+        } else if (numeroSecreto<numeroEscholido){
+              
+            alert(`o numero secreto é menor que ${numeroEscholido}`);
+   
+        } else{
+     
+            alert(`o numero secreto é maior que ${numeroEscholido}`);
 
-            alert(`o numero secreto é menor que ${numeroEscholido}`)
-
-         } else{
-
-            alert(`o numero secreto é maior que ${numeroEscholido}`)
-
-         }
-
+       }
 }
-
