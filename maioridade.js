e650cfc2e0732987f9d69c8db8e7f6b0a62c6f72
kaua-zanni <165018613+kaua-zanni@users.noreply.github.com>
2024-04-01 08:19:05 -0300

    up

--- maioridade.js
+++ maioridade.js
@@ -1,6 +1,6 @@
-let idade = parseInt(prompt('qual sua idade'))
+let idade = parseInt(prompt('qual sua idade'));
 if (idade >= 18) {
-    alert('você é maior de idade')
-}else {
-    alert('você é menor de idade')
+    alert('você é maior de idade');
+} else {
+    alert('você é menor de idade');
 }
