094f77ee5c963695ec71673760870dca97b929bc
kaua-zanni <165018613+kaua-zanni@users.noreply.github.com>
2024-04-05 09:15:39 -0300

    up

--- soma.js
+++ soma.js
@@ -1,15 +1,16 @@
 function media() {
 
-    let notas = []
-    let resultado= 0
+    let notas = [];
+    let sequenciaDeNota = [1,1,2,2];
+    let sequeciaDeProva = ['primeira','sequnda','terceira','quarta']
+    let resultado=0
     for (let index = 0; index < 4; index++) {
-        notas.push(parseInt(prompt('digite as notas')))
-        resultado = resultado + notas[index]
+        notas.push(parseInt(prompt(`digite a ${sequeciaDeProva[index]} nota`)));
+        resultado = resultado + notas[index]* sequenciaDeNota[index];
     }
-    resultado = resultado/4
+    resultado = resultado/6
 
-    alert(`${resultado}`)
-
+    alert(`${resultado}`);
 }
 function clicou() {
     media()
